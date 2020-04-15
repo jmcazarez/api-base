83c5bacde7f2268d263def4a9ce1f884e286357c
jmcazarez <63732249+jmcazarez@users.noreply.github.com>
2020-04-15 12:08:59 -0600

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,30 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-/* set up sql connection */
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host            : process.env.DATABASE_HOST,
-    port            : process.env.MYSQL_PORT,
-    user            : process.env.MYSQL_USER,
-    password        : process.env.MYSQL_PASSWORD,
-    database        : process.env.MYSQL_DATABASE
-});
-
-// /* throw an error if sql connect fails. it should fail a couple times in docker 
-//  before successfully connecting. the container takes longer to boot up, essentially.
-//  */
-connection.connect(function(err){
-	if(err){
-		console.error("error connecting: " + err.stack);
-		return process.exit(22); //consistently exit so the Docker container will restart until it connects to the sql db
-	}
-	console.log("connected as id " + connection.threadId);
-});
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  /* set up sql connection */
+  var mysql = require("mysql");
+  var connection = mysql.createConnection({
+      host            : process.env.DATABASE_HOST,
+      port            : process.env.MYSQL_PORT,
+      user            : process.env.MYSQL_USER,
+      password        : process.env.MYSQL_PASSWORD,
+      database        : process.env.MYSQL_DATABASE
+  });
+
+  // /* throw an error if sql connect fails. it should fail a couple times in docker 
+  //  before successfully connecting. the container takes longer to boot up, essentially.
+  //  */
+  connection.connect(function(err){
+    if(err){
+      console.error("error connecting: " + err.stack);
+      return process.exit(22); //consistently exit so the Docker container will restart until it connects to the sql db
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
   res.send(process.env.DATABASE_HOST);
   // var q = 'SELECT * from characters';
 	// var allegiance = 'select id, name from allegiance';
