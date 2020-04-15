054224ece458df8d5eb062286444ee599a0e4ce9
jmcazarez <63732249+jmcazarez@users.noreply.github.com>
2020-04-15 17:09:41 -0600

    ..

---
+++ api/db/db.js
@@ -0,0 +1,26 @@
+'user strict';
+
+var mysql = require('mysql');
+
+//local mysql db connection
+// var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : '',
+//     database : 'demo'
+// });
+
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host            : process.env.DATABASE_HOST,
+    port            : process.env.MYSQL_PORT,
+    user            : process.env.MYSQL_USER,
+    password        : process.env.MYSQL_PASSWORD,
+    database        : process.env.MYSQL_DATABASE
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
--- api/routes/usuarios.js
+++ api/routes/usuarios.js
@@ -1,70 +1,95 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/db');
 
-/* set up sql connection */
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host            : process.env.DATABASE_HOST,
-    port            : process.env.MYSQL_PORT,
-    user            : process.env.MYSQL_USER,
-    password        : process.env.MYSQL_PASSWORD,
-    database        : process.env.MYSQL_DATABASE
-});
-
-/* throw an error if sql connect fails. it should fail a couple times in docker 
- before successfully connecting. the container takes longer to boot up, essentially.
- */
-connection.connect(function(err){
-	if(err){
-		console.error("error connecting: " + err.stack);
-		return process.exit(22); //consistently exit so the Docker container will restart until it connects to the sql db
-	}
-	console.log("connected as id " + connection.threadId);
-});
-
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-    var q = 'SELECT * from characters';
-	var allegiance = 'select id, name from allegiance';
-	var ability = 'select id, type from ability';
-	var character = 'select id, first_name from characters';
-
-	/* query for characters, abilities, and allegiances then
-	pass the results into the page so the drop downs can be populated */
-	connection.query(allegiance, function(error, results, fields){
-		if(error) throw error;
-		allegiance = results;
-		//console.log(allegiance);
-		return;
-	});
-
-	connection.query(ability, function(error, results, fields){
-		if(error) throw error;
-		ability = results;
-		//console.log(ability);
-		return;
-	});
-
-	connection.query(character, function(error, results, fields){
-		if(error) throw error;
-		character = results;
-		//console.log(character);
-		return;
-	});
-
-
-	connection.query(q, function(error, results, fields){
-		if(error) throw error;
-		console.log("rendering home page . . .");
-		res.render('home', {
-			title: "Stormlight Archive DB",
-			results: results,
-			allegiance: allegiance,
-			ability: ability,
-			character: character
-		});
-    });
     
+    db.query("Select * from Usuarios", function (err, resx) {
+
+        if(err) {
+            console.log("error: ", err);
+            res.send(err);
+        }
+        else{
+            res.send(resx);
+        }
+    });   
+
 });
+
+router.get('/:id', function(req, res, next) {
+    
+    db.query("Select * from Usuarios where nIdUsuario = ?", req.params.id, function (err, resx) {
+
+        if(err) {
+            console.log("error: ", err);
+            res.send(err);
+        }
+        else{
+            res.send(resx);
+        }
+    });   
+
+});
+
+router.post('/', function(req, res, next) {
+    
+    var newUsuario = {
+        cLogin: req.body.cUsuario,
+        cNombre: req.body.cNombre,
+        cApellidoPaterno: req.body.cApellidoPaterno,
+        cApellidoMaterno: req.body.cApellidoMaterno,
+        cPassword: req.body.cPassword
+    };
+
+    db.query("INSERT INTO Usuarios set ?", newUsuario, function (err, resx) {       
+        if(err) {
+            console.log("error: ", err);
+            res.send(err);
+        }
+        else{
+            res.send(resx);
+        }
+    });   
+
+});
+
+router.put('/:id', function(req, res, next) {
+    
+    var updateUsuario = {
+        cLogin: req.body.cUsuario,
+        cNombre: req.body.cNombre,
+        cApellidoPaterno: req.body.cApellidoPaterno,
+        cApellidoMaterno: req.body.cApellidoMaterno,
+        cPassword: req.body.cPassword
+    };
+
+    db.query("UPDATE Usuarios SET cLogin = ?, cNombre = ?, cApellidoPaterno = ?, cApellidoMaterno = ?,  cPassword = ? WHERE nIdUsuario = ?", [updateUsuario.cLogin, updateUsuario.cNombre, updateUsuario.cApellidoPaterno, updateUsuario.cApellidoMaterno, updateUsuario.cPassword, req.params.id], function (err, resx) {       
+        if(err) {
+            console.log("error: ", err);
+            res.send(err);
+        }
+        else{
+            res.send(resx);
+        }
+    });   
+
+});
+
+router.delete('/:id', function(req, res, next) {
+    
+    db.query("DELETE FROM Usuarios WHERE nIdUsuario = ?", [req.params.id], function (err, resx) {
+
+        if(err) {
+            console.log("error: ", err);
+            res.send(err);
+        }
+        else{
+            res.send(resx);
+        }
+    });   
+
+});
+
 
 module.exports = router;
