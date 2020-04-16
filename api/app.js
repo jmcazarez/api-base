993c77799ceb3dd8ce3b28aadf188b2360904fe5
jmcazarez <63732249+jmcazarez@users.noreply.github.com>
2020-04-16 09:55:46 -0600

    ...

--- api/app.js
+++ api/app.js
@@ -3,12 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var usuariosRouter = require('./routes/usuarios');
 
 var app = express();
+
+// Configurar cabeceras y cors
+app.use(cors());
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
